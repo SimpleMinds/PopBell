33f3c19bff2b400310c69377837c42e921c6444e
sukso96100 <sukso96100@gmail.com>
2013-08-30 00:37:15 +0900

    Updated changes

--- PopBell/src/com/simpleminds/popbell/MainActivity.java
+++ PopBell/src/com/simpleminds/popbell/MainActivity.java
@@ -11,6 +11,9 @@ import android.content.SharedPreferences;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 	/** Called when the activity is first created. */
@@ -18,5 +21,13 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button setbtn = (Button)this.findViewById(R.id.button1);
+		setbtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);  
+				startActivity(intent);
+			}
+		});
 	}
 }
