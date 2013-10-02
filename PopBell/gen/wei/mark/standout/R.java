2b42c59a232717feaf40214f65d2fb15db2117ab
sukso96100 <sukso96100@gmail.com>
2013-10-02 22:35:24 +0900

    Replaced SlidingApneLayout with NavigationDrawer

--- PopBell/gen/wei/mark/standout/R.java
+++ PopBell/gen/wei/mark/standout/R.java
@@ -16,21 +16,21 @@ public final class R {
 		public static final int maximize = 0x7f020067;
 	}
 	public static final class id {
-		public static final int body = 0x7f0b006c;
-		public static final int close = 0x7f0b006b;
-		public static final int content = 0x7f0b0066;
-		public static final int corner = 0x7f0b006d;
+		public static final int body = 0x7f0b006f;
+		public static final int close = 0x7f0b006e;
+		public static final int content = 0x7f0b0069;
+		public static final int corner = 0x7f0b0070;
 		public static final int description = 0x7f0b004e;
-		public static final int hide = 0x7f0b0069;
+		public static final int hide = 0x7f0b006c;
 		public static final int icon = 0x7f0b0028;
-		public static final int maximize = 0x7f0b006a;
+		public static final int maximize = 0x7f0b006d;
 		public static final int title = 0x7f0b0029;
-		public static final int titlebar = 0x7f0b0067;
-		public static final int window_icon = 0x7f0b0068;
+		public static final int titlebar = 0x7f0b006a;
+		public static final int window_icon = 0x7f0b006b;
 	}
 	public static final class layout {
-		public static final int drop_down_list_item = 0x7f030026;
-		public static final int system_window_decorators = 0x7f030034;
+		public static final int drop_down_list_item = 0x7f030027;
+		public static final int system_window_decorators = 0x7f030035;
 	}
 	public static final class string {
 		public static final int close = 0x7f0a0002;
