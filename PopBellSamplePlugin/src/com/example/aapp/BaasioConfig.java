f6cf39f137bd045c952546fda097da7ed84ce96b
YoungBin Han <sukso96100@gmail.com>
2013-10-05 21:57:04 +0900

    Update BaasioConfig.java

--- PopBellSamplePlugin/src/com/example/aapp/BaasioConfig.java
+++ PopBellSamplePlugin/src/com/example/aapp/BaasioConfig.java
@@ -3,4 +3,13 @@ package com.example.aapp;
 public class BaasioConfig {
     public static final String BAASIO_URL = "https://api.baas.io";
 
+    // baas.io member ID
+    public static final String BAASIO_ID = "";
+
+    // baas.io application ID
+    public static final String APPLICATION_ID = "";
+
+    // GCM(google cloud messaging service) sender ID
+    public static final String GCM_SENDER_ID = "";
+
 }
